d3188b3e5e51beea5ec16384887cd8e56c55418b
Kai Davenport <kaiyadavenport@gmail.com>
2013-07-15 22:42:33 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,111 @@
+/*
+
+	(The MIT License)
+
+	Copyright (C) 2005-2013 Kai Davenport
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ */
+
+/*
+  Module dependencies.
+*/
+var _ = require('lodash');
+var url = require('url');
+var extend = require('extend');
+var uuid = require('node-uuid');
+
+var utils = module.exports = {};
+
+/**
+ * generate a new global id
+ */
+
+utils.diggerid = function(){
+  return uuid.v1().replace(/-/g, '');
+}
+
+utils.littleid = function(){
+
+  var pattern = 'xxxxxx';
+  
+  return pattern.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+}
+
+/**
+ * takes a string and prepares it to be used in a RegExp itself
+ */
+
+utils.escapeRegexp = function(search){
+  return search.replace(/([\!\$\(\)\*\+\.\/\:\=\?\[\\\]\^\{\|\}])/g, "\\$1");
+}
+
+/**
+ * Escape the given string of `html`.
+ *
+ * @param {String} html
+ * @return {String}
+ * @api private
+ */
+
+utils.escape = function(html){
+  return String(html)
+    .replace(/&(?!\w+;)/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;');
+};
+
+/**
+ * Parse the `req` url with memoization.
+ *
+ * @param {ServerRequest} req
+ * @return {Object}
+ * @api private
+ */
+
+utils.parseUrl = function(req){
+  var parsed = req._parsedUrl;
+  if (parsed && parsed.href == req.url) {
+    return parsed;
+  } else {
+    return req._parsedUrl = url.parse(req.url);
+  }
+};
+
+/*
+
+  make a route - optionally replace the '/'s with delimeter
+  
+*/
+utils.makeroute = function(parts, delimeter){
+
+  var route = _.filter(parts || [], function(part){
+    return _.isNumber(part) || !_.isEmpty(part);
+  }).join('/');
+
+  if(delimeter){
+    route = route.replace(/\//g, delimeter);
+    if(route.indexOf(delimeter)===0){
+      route = route.substr(1);
+    }
+  }
+
+  return route.replace(/\/\//g, '/');
+}
+
+
+
+/**
+ * jQuery Deep extend
+ */
+
+utils.extend = extend;
---
+++ test/digger-utils.test.js
@@ -0,0 +1,50 @@
+var utils = require('../src');
+
+describe('digger-utils', function(){
+
+	it('should produce a digger id', function() {
+		var id = utils.diggerid();
+
+		id.should.be.a('string');
+		id.length.should.equal(32);
+	})
+
+	it('should produce a littleid', function() {
+		var id = utils.littleid();
+
+		id.should.be.a('string');
+		id.length.should.equal(6);
+	})
+	it('should extend objects', function() {
+		var a = {
+			top:{
+				middle:{
+					bottom:{
+						vala:12
+					},
+					vala:12
+				},
+				vala:12
+			}
+			
+		}
+
+		var b = {
+			top:{
+				middle:{
+					bottom:{
+						valb:11
+					},
+					valb:11
+				},
+				valb:11
+			}
+		}
+
+		utils.extend(true, a, b);
+
+		a.top.valb.should.equal(11);
+		a.top.middle.valb.should.equal(11);
+		a.top.middle.bottom.valb.should.equal(11);
+  })
+})
